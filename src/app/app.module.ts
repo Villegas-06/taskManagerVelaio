27f2af7e2bbcec14aa7c2930cacc7e9543d4c9b8
miguelmontoya <miguel.angel@conexioncibernetica.com>
2024-09-29 10:14:06 -0500

    initial components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { PersonListComponent } from './components/person-list/person-list.component';
+import { PersonFormComponent } from './components/person-form/person-form.component';
+import { SkillFormComponent } from './components/skill-form/skill-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskFormComponent,
+    PersonListComponent,
+    PersonFormComponent,
+    SkillFormComponent
   ],
   imports: [
     BrowserModule,
